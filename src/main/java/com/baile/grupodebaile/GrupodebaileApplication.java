52254abe879a6ad8e0a8adc9fb38d9b057227315
Caren Salles <carencrisbsalles@gmail.com>
2023-03-27 12:44:47 +0200

    first commit

---
+++ src/main/java/com/baile/grupodebaile/GrupodebaileApplication.java
@@ -0,0 +1,13 @@
+package com.baile.grupodebaile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupodebaileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupodebaileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baile/grupodebaile/GrupodebaileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baile.grupodebaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupodebaileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
