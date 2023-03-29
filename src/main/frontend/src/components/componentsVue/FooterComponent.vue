43dca35f9c9ce5fc8c83e0d9aabe208d3fe80648
Caren Salles <carencrisbsalles@gmail.com>
2023-03-29 12:01:32 +0200

    footer finished

--- src/main/frontend/src/components/componentsVue/FooterComponent.vue
+++ src/main/frontend/src/components/componentsVue/FooterComponent.vue
@@ -8,13 +8,13 @@
     <div class="footer__container-title">
       <h2 class="footer__container-title__copyright">&copy; Xareu D' Ochobre</h2>
     </div>
-    <div class= "footer__map" style="width: 20%" height="5%"><iframe class="footer__map-iframe" width="100%" height="90%" frameborder="0" scrolling="no" marginheight="5%" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=C/El%20Alamo%20N%C2%BA2,%20Avil%C3%A9s,%20Spain+(Xar%C3%A9u%20D%20Ochobre%20Aviles)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">swimming watch</a></iframe></div>
     <div class="footer__container-social-media">
         
-        <p><a href=" https://www.facebook.com/profile.php?id=100051181520137" target="_blank"><img class= "icon-face" src="../../assets/images/icon-Facebook.png" alt="facebbok-icon"></a></p> 
-        <p><a href="https://instagram.com/xareu_d_ochobre?igshid=YmMyMTA2M2Y=" target="_blank"><img class="icon-insta" src="../../assets/images/icon-Instagram.png" alt="instagram-icon"></a></p> 
+        <p class="icons"><a href=" https://www.facebook.com/profile.php?id=100051181520137" target="_blank"><img class="image-icon" src="../../assets/images/iconFacebook.png" alt="facebbok-icon"></a></p> 
+        <p class="icons"><a href="https://instagram.com/xareu_d_ochobre?igshid=YmMyMTA2M2Y=" target="_blank"><img class="image-icon" src="../../assets/images/icon-Instagram.png" alt="instagram-icon"></a></p> 
  
   </div>
+   
   </footer>
 </template>
 
@@ -24,8 +24,10 @@
   background-color: #336644;
   display: flex;
   justify-content:space-around;
+  flex-wrap: wrap;
   color: white;
   align-items: center;
+  margin: 1vh;
   
   &__container-title{
      display: flex;
@@ -36,31 +38,22 @@
     }
 
 }
-  &__map{
-    width: 10%;
-    height: auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    margin-block: 1vh;
-    cursor: pointer;
-    &__iframe{
-      width: 20%;
-      height: auto;
-    }
-  }
-     
+
     &__container-social-media{
-      width: 1vw;
-      height: 1vh;
       display: flex;
       flex-direction: row;
       justify-content: flex-end;
-      gap: 1vw;
       cursor: pointer;
       
       }
-      
+      .icons{
+        margin: 1vh;
+        opacity: 100%;
+      }
+      .icons a:hover{
+          opacity:50%;
+      }
+
     }
 
 
@@ -69,9 +62,12 @@
 .footer {
   background-color: #336644;
   display: flex;
-  justify-content:space-around;
+  // justify-content:space-around;
+  flex-wrap: wrap;
   color: white;
   align-items: center;
+ 
+ 
   
   &__container-title{
      
@@ -81,26 +77,21 @@
     }
 
 }
-  &__map{
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    margin-block: 3vh;
-    cursor: pointer;
-  }
-     
+  
     &__container-social-media{
-      width: 5vw;
-      height: 5vh;
+     
       display: flex;
       flex-direction: row;
       justify-content: space-around;
-      gap: 2vw;
       cursor: pointer;
-      
+      max-height: 50%;
       }
-      
+
+    .icons {
+      margin: 1vh;
     }
-}
   
+  }
+    
+}
   </style>
