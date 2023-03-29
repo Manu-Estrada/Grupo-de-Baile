321de90aa69cfd3220403d978e886c83c1e73f36
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-03-29 14:50:36 +0200

    Fix assets/styles

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -5,5 +5,6 @@
 <template>
   <main>
     <img src="https://www.infojobs.net/ficha.foto?quina=60587ABF-E7F3-0921-59048C09E080349C" alt="yonih" />
+    <h5 class="h1">Hola</h5>
   </main>
 </template>
