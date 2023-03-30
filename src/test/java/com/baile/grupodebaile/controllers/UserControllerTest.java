05d999c846a9e15e87c8618679a992532f5b9e87
Xavi <xavi9415@hotmail.com>
2023-03-30 14:49:32 +0200

    test user controller

---
+++ src/test/java/com/baile/grupodebaile/controllers/UserControllerTest.java
@@ -0,0 +1,59 @@
+package com.baile.grupodebaile.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.baile.grupodebaile.models.User;
+import com.baile.grupodebaile.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(value = UserController.class)
+public class UserControllerTest {
+    
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService service;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    @WithMockUser("userTest")
+    public void testRetrieveAllUsers() throws Exception {
+
+        List<User> users = new ArrayList<User>();
+        User user1 = new User();
+        User user2 = new User();
+
+        users.add(user1);
+        users.add(user2);
+
+        when(service.listAll()).thenReturn(users);
+        MockHttpServletResponse response = this.mockMvc.perform(MockMvcRequestBuilders.get("/api/users"))
+        .andExpect(status().isOk())
+        .andReturn()
+        .getResponse();
+        
+        System.out.println(response.getContentAsString());
+
+        assertEquals(response.getStatus(), 200);
+        // assertEquals(response.getContentAsString(), "[{\"user_id\":\"1\"},{\"user_id\":\"2\"}]");
+        assertEquals(response.getContentAsString(), mapper.writeValueAsString(users));
+    }
+}
