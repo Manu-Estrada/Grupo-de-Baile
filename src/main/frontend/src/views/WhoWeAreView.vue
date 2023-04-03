4228e8ffcf4a57b41db60f344ecd1e8135713cf8
JehiselRuth <jehisel.rrp@gmail.com>
2023-04-03 09:01:30 +0200

    cambios componente Quienes somos

--- src/main/frontend/src/views/WhoWeAreView.vue
+++ src/main/frontend/src/views/WhoWeAreView.vue
@@ -4,7 +4,23 @@ import WhoWeAre from '../components/WhoWeAre.vue';
 
 <template>
   <main>
+  <h2>Sobre nosotros</h2>
    <h5>PaNJOLIN</h5>
    <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+   <WhoWeAre />
+
   </main>
 </template>
+<style>
+h2 {
+  color: #cc0033;
+  margin-top: 2em;
+}
+</style>
