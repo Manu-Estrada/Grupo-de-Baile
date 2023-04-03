719fd6eda1f7561d95ca9af9347441ae685a7d87
JehiselRuth <jehisel.rrp@gmail.com>
2023-04-03 10:49:37 +0200

    Who we are component styles

--- src/main/frontend/src/components/WhoWeAre.vue
+++ src/main/frontend/src/components/WhoWeAre.vue
@@ -5,20 +5,30 @@
 <template>
   <main>
    <div class="card">
-    <img src="../assets/images/foto1.webp" alt="Imagen" />
-  <div class="card-body pt-1">
-    <p class="card-text"><small class="text-muted">01-01-2023</small></p>
-    <h5 class="card-title">Josefa García</h5>
-    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-    <div id="containerBtn">
-      <a href="#" class="btn btn-primary" id="Btn-see">Ampliar</a>
+    <div class="modal-dialog modal-fullscreen-sm-down">
+      <img  src="../assets/images/foto3.jpg" alt="Imagen" />
     </div>
-  </div>
-</div>
+    <div class="card-body pt-1">
+      <p class="card-text"><small class="text-muted">01-01-2023</small></p>
+      <h5 class="card-title">José García</h5>
+      <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+      <div id="containerBtnMemebrs">
+        <a href="#" class="btn btn-primary" id="Btn-see" >Ampliar</a>
+      </div>
+    </div>
+   </div>
   </main>
 </template>
 <style lang="scss" scoped>
-
+@import "../assets/sass/_variables.scss";
 @import "../assets/sass/galleryStyles/gallerystyles.scss";
+
+.card  {
+
+  #containerBtnMemebrs{
+      display: flex;
+      justify-content: center;
+      }
+}
 
 </style>
