e7d2ad545b9cef2e8068df05beb88b3a9f0f7c91
Xavi1594 <xavi9415@hotmail.com>
2023-04-03 14:11:34 +0200

    formulario registro nuevo miembro

--- src/main/frontend/src/components/componentsVue/FooterComponent.vue
+++ src/main/frontend/src/components/componentsVue/FooterComponent.vue
@@ -1,35 +1,25 @@
 <script setup></script>
-
 <template>
   <footer class="footer w-100">
     <div class="footer__container-title">
-      <h2 class="footer__container-title__copyright">&copy; Xaréu D'Ochobre</h2>
+      <h2 class="footer__container-title__copyright">
+        &copy; Xaréu D'Ochobre
+      </h2>
     </div>
     <div class="footer__container-social-media">
       <p class="icons">
-        <a
-          href=" https://www.facebook.com/profile.php?id=100051181520137"
-          target="_blank"
-          ><img
-            class="image-icon"
-            src="../../assets/images/iconFacebook.png"
-            alt="facebbok-icon"
-        /></a>
+        <a href="https://www.facebook.com/profile.php?id=100051181520137" target="_blank">
+          <img class="image-icon" src="../../assets/images/iconFacebook.png" alt="facebbok-icon"/>
+        </a>
       </p>
       <p class="icons">
-        <a
-          href="https://instagram.com/xareu_d_ochobre?igshid=YmMyMTA2M2Y="
-          target="_blank"
-          ><img
-            class="image-icon"
-            src="../../assets/images/icon-Instagram.png"
-            alt="instagram-icon"
-        /></a>
+        <a href="https://instagram.com/xareu_d_ochobre?igshid=YmMyMTA2M2Y=" target="_blank">
+          <img class="image-icon" src="../../assets/images/icon-Instagram.png" alt="instagram-icon"/>
+        </a>
       </p>
     </div>
   </footer>
 </template>
-
 <style lang="scss" scoped>
 .footer {
   background-color: #336644;
@@ -38,6 +28,12 @@
   flex-wrap: wrap;
   color: white;
   align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  display: none;
 
   &__container-title {
     display: flex;
@@ -54,10 +50,12 @@
     justify-content: flex-end;
     cursor: pointer;
   }
+
   .icons {
     margin: 1vh;
     opacity: 100%;
   }
+
   .icons a:hover {
     opacity: 50%;
   }
@@ -88,6 +86,13 @@
     .icons {
       margin: 1vh;
     }
+  }
+}
+
+
+@media (min-width: 1024px) {
+  .footer {
+    display: flex;
   }
 }
 </style>
