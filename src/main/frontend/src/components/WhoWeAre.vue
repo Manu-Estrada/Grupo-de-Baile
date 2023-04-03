daf30401e5ade0a16cc133edae5782caba37fcc4
JehiselRuth <jehisel.rrp@gmail.com>
2023-04-03 14:08:19 +0200

    pop up working

--- src/main/frontend/src/components/WhoWeAre.vue
+++ src/main/frontend/src/components/WhoWeAre.vue
@@ -9,13 +9,26 @@
       <div class="card-body pt-1">
         <p class="card-text"><small class="text-muted">01-01-2023</small></p>
         <h5 class="card-title">José García</h5>
-        <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
       <div id="containerBtnMemebrs">
-        <a href="#" class="btn btn-primary" id="Btn-see" >Ampliar</a>
+        <a data-bs-toggle="modal"  data-bs-target="#exampleModal" class="btn btn-primary" id="Btn-see" >Ampliar</a>
       </div>
     </div>
    </div>
   </main>
+
+<!-- ampliar imagen -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" id="modalDialog">
+    <div class="modal-content bg-transparent" id="boxImg">
+        <div class="d-flex justify-content-end">
+        <button  type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="btnClosed" ></button>
+      </div>
+        <img src="https://lh5.googleusercontent.com/_T4ZV2_ZON54aqxKpkWqnwVa8u43Sfe0c0aM5PWoktF_vGqp5xDmVCR_Fpfys_UQmtM=w1200-h630-p" width="1357px" height="650px" data-bs-toggle="modal" data-bs-target="#exampleModal"/>
+    </div>
+  </div>
+</div>
+
+
 </template>
 <style lang="scss" scoped>
 @import "../assets/sass/_variables.scss";
