7b814e3f7ea20620ec176ac9686458818cff67f7
JehiselRuth <jehisel.rrp@gmail.com>
2023-04-05 09:27:14 +0200

    pagination

--- src/main/frontend/src/views/WhoWeAreView.vue
+++ src/main/frontend/src/views/WhoWeAreView.vue
@@ -3,7 +3,38 @@ import WhoWeAre from "../components/WhoWeAre.vue";
 import { onBeforeMount, ref } from "vue";
 import ApiRepository from "./../assets/ApiRepository/ApiRepository.js";
 import { computed } from "@vue/reactivity";
+//Paginacion
+import BlogPost from "./components/BlogPost.vue"
+import PaginatePost from "./components/PaginatePost.vue"
+import LoadingSpinner from "./components/LoadingSpinner.vue"
 
+const posts = ref([])
+const postXPagina = 10
+const inicio = ref(0)
+const fin = ref(postXPagina)
+const loading = ref(true)
+ 
+const favorito = ref('')
+
+const cambiarFavorito = (title) => {
+  favorito.value = title
+};
+const next = () => {
+  inicio.value = inicio.value + postXPagina
+  fin.value = fin.value + postXPagina
+}
+const prev = () => {
+  inicio.value = inicio.value - postXPagina
+  fin.value = fin.value - postXPagina
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(respuesta => respuesta.json())
+.then(data => posts.value = data )
+.finally(() => loading.value = false)
+
+
+//Api
 const repository = new ApiRepository("quienesSomos");
 const api = repository.chooseApi();
 
@@ -14,6 +45,11 @@ onBeforeMount(async () => {
 const showThisMember = computed(() => {
   return membersList;
 });
+
+// Xavi
+defineProps(['inicio','fin','maxLength'])
+const emit = defineEmits(['next','prev']) 
+
 </script>
 
 <template>
@@ -26,13 +62,10 @@ const showThisMember = computed(() => {
         <WhoWeAre v-for="member in showThisMember.value" :member="member" />
       </div>
     </div>
-
-   
-
   </main>
 
   <nav aria-label="Page navigation example">
-  <ul class="pagination">
+  <ul class="pagination pagination-lg" style="justify-content: center; margin:20px 0;">
     <li class="page-item">
       <a class="page-link" href="#" aria-label="Previous">
         <span aria-hidden="true">&laquo;</span>
@@ -41,6 +74,8 @@ const showThisMember = computed(() => {
     <li class="page-item"><a class="page-link" href="#">1</a></li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">4</a></li>
+
     <li class="page-item">
       <a class="page-link" href="#" aria-label="Next">
         <span aria-hidden="true">&raquo;</span>
@@ -48,6 +83,18 @@ const showThisMember = computed(() => {
     </li>
   </ul>
 </nav>
+
+<!-- <div class="btn-group" role="group" aria-label="Basic example">
+    <button
+      @click="emit('prev')"
+      type="button"
+      class="btn btn-primary"
+      :disabled='inicio <= 0'>
+      Anterior {{inicio}}
+    </button>
+
+    <button @click="emit('next')" class="btn btn-primary" :disabled="fin >= 100">Siguiente {{fin}}</button>
+  </div> -->
 </template>
 <style scoped lang="scss">
 @import "../assets/sass/variables";
@@ -67,10 +114,12 @@ const showThisMember = computed(() => {
 }
 #headerH3 {
   width: 90%;
-  // margin: 0px 50px 10px 53px;
 }
 h3 {
   font-weight: bold;
   margin: 0.5em;
+}
+#color-pag  {
+  color: black;
 }
 </style>
