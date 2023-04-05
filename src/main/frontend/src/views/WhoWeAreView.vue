b1398c33154666f0ccbff860f883a986a326bedf
JehiselRuth <jehisel.rrp@gmail.com>
2023-04-05 14:01:28 +0200

    init pagination

--- src/main/frontend/src/views/WhoWeAreView.vue
+++ src/main/frontend/src/views/WhoWeAreView.vue
@@ -3,6 +3,9 @@ import WhoWeAre from "../components/WhoWeAre.vue";
 import { onBeforeMount, ref } from "vue";
 import ApiRepository from "./../assets/ApiRepository/ApiRepository.js";
 import { computed } from "@vue/reactivity";
+//paginación
+defineProps(['start','end','maxLength'])
+const emit = defineEmits(['next','prev']) 
 
 //Api
 const repository = new ApiRepository("quienesSomos");
@@ -16,9 +19,27 @@ const showThisMember = computed(() => {
   return membersList;
 });
 
-//Paginacion
-defineProps(['inicio','fin','maxLength'])
-const emit = defineEmits(['next','prev']) 
+//Paginación
+const memberCard = ref([])
+//Número de miembros por página (2)
+const memberCardxPage = 2
+const start = ref(0)
+const end = ref(memberCardxPage)
+
+const next = () => {
+  start.value = start.value + memberCardxPage
+  end.value = end.value + memberCardxPage
+}
+
+const prev = () => {
+  start.value = start.value - memberCardxPage
+  end.value = end.value - memberCardxPage
+}
+
+fetch('http://localhost:8080/api/quienessomos')  
+.then(response => response.json())
+.then(data => memberCard.value = data)
+console.log(data);
 
 </script>
 
@@ -29,7 +50,7 @@ const emit = defineEmits(['next','prev'])
         <h3>Quiénes somos</h3>
       </div>
       <div id="containerAlbums">
-        <WhoWeAre v-for="member in showThisMember.value" :member="member" />
+        <WhoWeAre v-for="member in showThisMember.value.slice(start, end)" :member="member" />
       </div>
     </div>
   </main>
@@ -37,7 +58,7 @@ const emit = defineEmits(['next','prev'])
   <nav aria-label="Page navigation example">
   <ul class="pagination pagination-lg" style="justify-content: center; margin:20px 0;">
     <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
+      <a class="page-link" href="#" aria-label="Previous" @prev = 'prev' :start = 'start'>
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
@@ -47,7 +68,7 @@ const emit = defineEmits(['next','prev'])
     <li class="page-item"><a class="page-link" href="#">4</a></li>
 
     <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
+      <a class="page-link" href="#" aria-label="Next" @next = 'next' :end="end">
         <span aria-hidden="true">&raquo;</span>
       </a>
     </li>
