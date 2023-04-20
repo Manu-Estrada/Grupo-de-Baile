d24dc6f2b28793cc4bd8dd624c6b71eb1a7cab8b
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-04-20 15:21:28 +0200

    Merge

--- src/main/java/com/baile/grupodebaile/controllers/TravelController.java
+++ src/main/java/com/baile/grupodebaile/controllers/TravelController.java
@@ -1,6 +1,7 @@
 package com.baile.grupodebaile.controllers;
 
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import com.baile.grupodebaile.models.Travel;
 import com.baile.grupodebaile.services.TravelService;
